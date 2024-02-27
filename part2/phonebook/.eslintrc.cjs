8318c54ef8620c2419fb813905d4010a04eb70a6
yarivigano <yari.vigano@inmagik.com>
2024-02-27 10:45:53 +0100

    lint changes

--- part2/phonebook/.eslintrc.cjs
+++ part2/phonebook/.eslintrc.cjs
@@ -17,6 +17,15 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    'react/prop-types': 0
+    'react/prop-types': 0,
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+        'error', 'always'
+    ],
+    'arrow-spacing': [
+        'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 0
   },
 }
--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,27 +1,27 @@
-import axios from 'axios'
+import axios from "axios";
 // const baseUrl = 'http://localhost:3001/persons' //singolo esercizio
 // const baseUrl = 'http://localhost:3001/api/phonebook' //insieme al server absolute
-const baseUrl = '/api/phonebook' //insieme al server relative
+const baseUrl = "/api/phonebook"; //insieme al server relative
 
 const getAll = () => {
-  return axios.get(baseUrl)
-}
+  return axios.get(baseUrl);
+};
 
-const create = newObject => {
-  return axios.post(baseUrl, newObject)
-}
+const create = (newObject) => {
+  return axios.post(baseUrl, newObject);
+};
 
 const update = (id, newObject) => {
-  return axios.put(`${baseUrl}/${id}`, newObject)
-}
+  return axios.put(`${baseUrl}/${id}`, newObject);
+};
 
 const deletePerson = (id) => {
-  return axios.delete(`${baseUrl}/${id}`)
-}
+  return axios.delete(`${baseUrl}/${id}`);
+};
 
-export default { 
-  getAll: getAll, 
-  create: create, 
+export default {
+  getAll: getAll,
+  create: create,
   update: update,
-  deletePerson: deletePerson
-}
+  deletePerson: deletePerson,
+};
