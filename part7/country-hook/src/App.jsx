dc0881eb77908281ff121140c11b468854e2ec5c
yarivigano <yari.vigano@inmagik.com>
2024-05-09 12:07:11 +0200

    Exercises 7.7

---
+++ part7/country-hook/src/App.jsx
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+import countries from "../services/countries";
+
+const useField = (type) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return {
+    type,
+    value,
+    onChange,
+  };
+};
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null);
+  useEffect(() => {
+    countries
+      .getCountry(name)
+      .then((response) => {
+        setCountry({ ...response.data, found: true });
+      })
+      .catch(() => {
+        setCountry({found: false});
+      });
+  }, [name]);
+
+  return country;
+};
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null;
+  }
+
+  if (!country.found) {
+    return <div>not found...</div>;
+  }
+
+  return (
+    <div>
+      <h3>{country.name.common} </h3>
+      <div>capital {country.capital[0]} </div>
+      <div>population {country.population}</div>
+      <img
+        src={country.flags.svg}
+        height="100"
+        alt={`flag of ${country.name.common}`}
+      />
+    </div>
+  );
+};
+
+const App = () => {
+  const nameInput = useField("text");
+  const [name, setName] = useState("");
+  const country = useCountry(name);
+
+  console.log(country);
+  const fetch = (e) => {
+    e.preventDefault();
+    setName(nameInput.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  );
+};
+
+export default App;
