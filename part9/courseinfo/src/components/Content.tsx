923687e28df00501b27a49ae2f58e26090fef3ea
yarivigano <yari.vigano@inmagik.com>
2024-05-15 14:09:19 +0200

    exercises 9.14

---
+++ part9/courseinfo/src/components/Content.tsx
@@ -0,0 +1,19 @@
+import { CourseParts } from "../types"
+
+const Content = ({ courseParts }: { courseParts: CourseParts[] }) => {
+  return (
+    <div>
+      <p>
+        {courseParts[0].name} {courseParts[0].exerciseCount}
+      </p>
+      <p>
+        {courseParts[1].name} {courseParts[1].exerciseCount}
+      </p>
+      <p>
+        {courseParts[2].name} {courseParts[2].exerciseCount}
+      </p>
+    </div>
+  )
+}
+
+export default Content
