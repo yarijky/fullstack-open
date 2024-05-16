16ed5c391a9dbc1d5cd1be81c56666c22a9faa0d
yarivigano <yari.vigano@inmagik.com>
2024-05-16 10:38:39 +0200

    Exercises 9.16-19

--- part9/flight-diary/frontend/src/App.tsx
+++ part9/flight-diary/frontend/src/App.tsx
@@ -1,15 +1,16 @@
-import { useState, useEffect } from "react";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 import { apiBaseUrl } from "../constants";
 import { DiaryEntry, NewDiaryEntry } from "./types";
 
-import diaryServices from "./services/diary";
-import DiariesList from "./components/DiariesList";
 import DiariesAdd from "./components/DiariesAdd";
+import DiariesList from "./components/DiariesList";
+import diaryServices from "./services/diary";
 
 const App = () => {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+  const [errorMessage, setErrorMessage] = useState<string>();
 
   useEffect(() => {
     void axios.get<void>(`${apiBaseUrl}/ping`);
@@ -18,19 +19,28 @@ const App = () => {
       const diaries = await diaryServices.getAll();
       setDiaries(diaries);
     };
-    void fetchDiariesList(); 
+    void fetchDiariesList();
   }, []);
 
   const onAddDiary = (newDiary: NewDiaryEntry): void => {
-    const fetchNewDiaryPost = async () => {
-      const diary = await diaryServices.create(newDiary)
-      setDiaries(diaries.concat(diary));
+    try {
+      const fetchNewDiaryPost = async () => {
+        const diary = await diaryServices.create(newDiary);
+        setDiaries(diaries.concat(diary));
+      };
+      void fetchNewDiaryPost();
+    } catch (e) {
+      if (axios.isAxiosError(e)) {
+        setErrorMessage(e.response?.data);
+      } else {
+        console.error(e);
+      }
     }
-    void fetchNewDiaryPost()
   };
 
   return (
     <div>
+      {errorMessage && <div>{errorMessage}</div>}
       <DiariesAdd onClick={onAddDiary} />
       <DiariesList diaries={diaries} />
     </div>
